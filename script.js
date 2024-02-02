9a5a4599853df7c7050547eaa3e8179b515e8134
Joel Berg <bergjoel93@gmail.com>
2024-02-02 16:31:37 -0700

    rewrote a lot of the logic. This code is different than previous version.

--- script.js
+++ script.js
@@ -3,32 +3,43 @@
 let firstNumber = 0;
 let operator = '';
 let secondNumber = '';
-let numButtonClicked = false; // keep track if a numButton has been clicked
+let solution = '';
+//let numButtonClicked = false; // keep track if a numButton has been clicked
 let operatorButtonClicked = false; // keep track if an operatorButton has been clicked. 
-updateDisplay("0");
-//Program Logic
-// Initialize state and bind event listeners. 
+updateDisplay(firstNumber);
+// keep track of whether first number or second number is active and being displayed. 
+let firstNumberActive = true; // default state is true 
+let secondNumberActive = false;
+let equalsButtonClicked = false;
+
+//////////////////Program Logic////////////////////////////
+
+// Initialize state and bind event listeners for firstNumber and secondNumber
 let numBtns = document.querySelectorAll("button.num");
 numBtns.forEach(btn => {
     btn.addEventListener('click', ()=>{
-        //check if operator Button clicked
-        if(operatorButtonClicked == false){
-            // check if number button has been clicked yet
-            if(numButtonClicked == false || firstNumber == 0){
+        //if(solution !='') firstNumber = 0;
+        if(equalsButtonClicked && !operatorButtonClicked){
+            clear();
+            equalsButtonClicked == false;
+            console.log("equalsButtonClicked: "+equalsButtonClicked);
+        }
+
+        if(firstNumberActive){
+            if(firstNumber == 0){ 
                 firstNumber = btn.value;
                 updateDisplay(firstNumber);
-                numButtonClicked = true;
                 console.log("firstNumber: "+firstNumber);
             }
-            else {
+            else { //firstNumber is already active 
                 firstNumber += btn.value;
                 updateDisplay(firstNumber);
                 console.log("firstNumber: "+firstNumber);
             }
         }
-        else if(operatorButtonClicked == true) {
-            //check to see if secondNumber is empty
-            if(secondNumber == '' || secondNumber == 0){
+
+        if(secondNumberActive){
+            if(secondNumber == 0 || secondNumber == ''){
                 secondNumber = btn.value;
                 updateDisplay(secondNumber);
                 console.log("secondNumber: "+secondNumber);
@@ -44,22 +55,45 @@ numBtns.forEach(btn => {
 
 // clear button
 let clearBtn = document.querySelector("#clear");
-clearBtn.addEventListener('click', ()=>{
-    firstNumber = 0;
-    operator = '';
-    secondNumber = '';
-    numButtonClicked = false; 
-    operatorButtonClicked = false; 
-    updateDisplay("0");
-    console.log("Calculator cleared!");
+clearBtn.addEventListener('click', () =>{
+    clear();
+    equalsButtonClicked = false;
 });
 
 // operate button
 let operateButtons = document.querySelectorAll(".operator");
 operateButtons.forEach(btn => {
     btn.addEventListener('click', ()=>{
-        operator = btn.value;
-        operatorButtonClicked = true;
+        equalsButtonClicked = false;
+        if(!operatorButtonClicked){
+            operatorButtonClicked = true;
+            operator = btn.value;
+            btn.classList.add("active");
+            firstNumberActive = false;
+            secondNumberActive = true;
+        }
+        else if(operatorButtonClicked && secondNumber !=''){
+           firstNumber = operate(Number(firstNumber), operator, Number(secondNumber));
+           secondNumber = '';
+           console.log("firstNumber: "+firstNumber);
+           removeActive();
+           operator = btn.value;
+           btn.classList.add("active");
+           updateDisplay(firstNumber);
+           firstNumberActive = false;
+           secondNumberActive = true;
+        }
+        else if(operatorButtonClicked && secondNumber ==''){
+            firstNumber = operate(Number(firstNumber), operator, Number(firstNumber));
+           secondNumber = '';
+           console.log("firstNumber: "+firstNumber);
+           removeActive();
+           operator = btn.value;
+           btn.classList.add("active");
+           updateDisplay(firstNumber);
+           firstNumberActive = false;
+           secondNumberActive = true;
+        }
         console.log("operator: "+ operator);
     });
 });
@@ -67,11 +101,25 @@ operateButtons.forEach(btn => {
 // equals button
 let equalsButton = document.querySelector("#equals");
 equalsButton.addEventListener('click', ()=> {
-    if(operator==''){
-        updateDisplay(firstNumber);
+    if(!equalsButtonClicked && (operator == '' || secondNumber =='')){
+        clear();
+        equalsButtonClicked = false;
     }
-    operate(Number(firstNumber), operator, Number(secondNumber));
+    else if(!equalsButtonClicked){
+        equalsButtonClicked=true;
+        console.log("equalsButtonClicked: "+equalsButtonClicked);
+        solution = operate(Number(firstNumber), operator, Number(secondNumber));
+        clear();
+        firstNumber = solution;
+        console.log("firstNumber: "+firstNumber)
+        updateDisplay(solution);
+    }
+        
 });
+
+
+
+
 
 
 ////////// FUNCTIONS ///////////
@@ -95,12 +143,13 @@ function operate(num1, op, num2){
         default:
         updateDisplay(error);
     }
-    updateDisplay(solution);
-    operatorButtonClicked = false;
-    operator = '';
-    secondNumber = '';
-    firstNumber = solution;
-    console.log("First number updated: "+firstNumber);
+    return solution;
+    // updateDisplay(solution);
+    // operatorButtonClicked = false; //TODO possible bug might need to add conditional 
+    // operator = '';
+    // secondNumber = '';
+    // firstNumber = solution;
+    // console.log("First number updated: "+firstNumber);
 }
 
 //whatever you want to be displayed will be an argument of updateDisplay
@@ -111,3 +160,22 @@ function updateDisplay(value) {
     display.textContent = roundedNum;
 }
 
+function removeActive(){
+    const activeElement = document.querySelector(".active");
+    if(activeElement) { // check if an element with the class 'active' exists. 
+        activeElement.classList.remove("active");
+    }
+}
+
+function clear(){ 
+    firstNumber = 0;
+    operator = '';
+    secondNumber = '';
+    firstNumberActive = true;
+    secondNumberActive = false;
+    //equalsButtonClicked = false;
+    operatorButtonClicked = false; 
+    updateDisplay("0");
+    removeActive();
+    console.log("Calculator cleared!");
+}
